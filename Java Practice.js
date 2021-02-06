03476d75a51e6e26e970eedc8fc14aa42fe965e9
etenhagen04 <74108427+etenhagen04@users.noreply.github.com>
2021-02-05 22:50:18 -0500

    Add files via upload

---
+++ Java Practice.js
@@ -0,0 +1,14 @@
+const people = {
+    person1: 'Mom',
+    person2: 'Ezekiel',
+    person3: 'Danny'
+}
+
+let home = {
+    state: 'New Jersey',
+    city: 'Union City',
+    address: '8th Street',
+    homeType: 'Apartment',
+    bedrooms: 3,
+    bathrooms: 2
+}
